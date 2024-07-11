6bb10a295417c708094aa1ba100e6a32720ff24d
fawadsw20 <fawadhassan.sw1997@gmail.com>
2024-07-11 11:34:26 +0500

    Add files via upload

---
+++ Object de-structure/script.js
@@ -0,0 +1,12 @@
+const course = {
+
+    coursenamme: "JS in Urdu", 
+    Price: "999",
+    courseInstructor: "hitesh"
+}
+
+
+
+const {courseInstructor: Instructor} = course;
+
+console.log(Instructor);
